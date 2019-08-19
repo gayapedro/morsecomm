497c3de5e20d6571223895972ae73646c0270907
Pedro Gaya <pedrogaya@gmail.com>
2019-08-19 14:47:06 -0300

    Add files via upload

--- printMorse.ino
+++ printMorse.ino
@@ -1,5 +1,6 @@
 #define led 8
-#define dotduration 1000
+#define dotduration 300
+unsigned long time;
 String textToMorse;
 String charCode;
 String digits[36] = {".-","-...","-.-.","-..",".","..-.",
@@ -9,7 +10,6 @@ String digits[36] = {".-","-...","-.-.","-..",".","..-.",
                       "-.--","--..","-----",".----","..---","...--",
                       "....-",".....","-....","--...","---..","----."};
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(led, OUTPUT);
   charCode = "";
@@ -17,16 +17,43 @@ void setup() {
 }
 
 void dotFlash(){
-  
+  time = millis();
+  digitalWrite(led, HIGH);
+  while(millis()<(time+dotduration));
+  digitalWrite(led, LOW);
+}
+
+void dashFlash(){
+  time = millis();
+  digitalWrite(led, HIGH);
+  while(millis()<(time+dotduration*3));
+  digitalWrite(led, LOW);
+}
+
+void interval(){
+  time = millis();
+  while(millis()<(time+dotduration));
+}
+
+void space() {
+  time = millis();
+  while(millis()<(time+dotduration*7));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   if(Serial.available()>0){
     textToMorse = Serial.readString();
     for(int i=0;i<textToMorse.length();i++){
       if(isDigit(textToMorse[i])) charCode = digits[textToMorse[i]-22];
       else charCode = digits[textToMorse[i]-97];
+      for(int j=0;j<charCode.length();j++){
+        if(charCode[j]=='.') dotFlash();
+        else dashFlash();
+        if(j != charCode.length()-1) interval();
+        else{
+          if(i != textToMorse.length()-1) space();
+        }
+      }
     }
   }
 }
