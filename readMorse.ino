6ff591c7630aa862a8899de5e2952a1b8fb5efc2
Pedro Gaya <pedrogaya@gmail.com>
2019-08-19 16:55:11 -0300

    Add files via upload

---
+++ readMorse.ino
@@ -0,0 +1,34 @@
+#define dotduration 300
+unsigned long time;
+int LDRpin = A5;
+int durationOn = 0;
+int durationOff = 0;
+String phrase = "";
+String charCode = "";
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(A5, INPUT);
+}
+
+void loop() {
+  time = millis();
+  while(analogRead(LDRpin)<=110);
+  Serial.println(analogRead(LDRpin));
+  durationOn = millis()-time;
+  if(durationOn>(dotduration-10)&&durationOn<(dotduration+10)){
+    charCode += ".";
+    Serial.print(charCode);
+  }else if(durationOn>(3*dotduration-10)&&durationOn<(3*dotduration+10)){
+    charCode += "-";
+    Serial.print(charCode);
+  }
+  time = millis();
+  while(analogRead(LDRpin)>=150&&(millis()-time<5*dotduration));
+  durationOff = millis()-time;
+  if(durationOff >= 5*dotduration){
+    //Serial.print(charCode);
+    charCode = "";
+  }
+  delay(500);
+}
